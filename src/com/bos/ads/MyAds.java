e93a8854796e649bf2e7e11c8b3be6aaa9e543b4
bansos567 <notopribadi25@gmail.com>
2026-05-17 22:40:16 +0700

    Update MyAds.java

--- src/com/bos/ads/MyAds.java
+++ src/com/bos/ads/MyAds.java
@@ -5,117 +5,116 @@ import android.content.Context;
 import com.google.appinventor.components.annotations.*;
 import com.google.appinventor.components.runtime.*;
 
-import com.unity3d.ads.IUnityAdsInitializationListener;
-import com.unity3d.ads.IUnityAdsLoadListener;
-import com.unity3d.ads.IUnityAdsShowListener;
-import com.unity3d.ads.UnityAds;
-import com.unity3d.ads.UnityAdsShowOptions;
+// Import library Start.io
+import com.startapp.sdk.adsbase.StartAppAd;
+import com.startapp.sdk.adsbase.StartAppSDK;
+import com.startapp.sdk.adsbase.adlisteners.AdDisplayListener;
+import com.startapp.sdk.adsbase.adlisteners.AdEventListener;
+import com.startapp.sdk.adsbase.Ad;
 
 @DesignerComponent(
     version = 1,
-    description = "Ekstensi Unity Ads Interstitial",
+    description = "Ekstensi Start.io Interstitial Anti Bentrok",
     iconName = "images/extension.png",
     nonVisible = true
 )
 @SimpleObject(external = true)
-@UsesPermissions(permissionNames = "android.permission.INTERNET, android.permission.ACCESS_NETWORK_STATE")
+// Izin internet langsung disuntik di sini agar tidak ribet
+@UsesPermissions(permissionNames = "android.permission.INTERNET, android.permission.ACCESS_NETWORK_STATE, android.permission.ACCESS_WIFI_STATE")
 public class MyAds extends AndroidNonvisibleComponent {
 
     private Context context;
-    private Activity activity;
+    private StartAppAd startAppAd;
 
     public MyAds(ComponentContainer container) {
         super(container.$form());
         this.context = container.$context();
-        this.activity = (Activity) context;
     }
 
-    @SimpleFunction(description = "Mulai koneksi ke Unity Ads pakai Game ID")
-    public void Initialize(String gameId, boolean testMode) {
-        UnityAds.initialize(context, gameId, testMode, new IUnityAdsInitializationListener() {
+    // 1. INISIALISASI (Cukup 1 App ID)
+    @SimpleFunction(description = "Inisialisasi Start.io pakai App ID")
+    public void Initialize(String appId) {
+        StartAppSDK.init(context, appId, false);
+        // Siapkan wadah untuk iklannya
+        startAppAd = new StartAppAd(context);
+    }
+
+    // 2. LOAD IKLAN
+    @SimpleFunction(description = "Load Iklan Interstitial")
+    public void LoadInterstitial() {
+        if (startAppAd == null) {
+            startAppAd = new StartAppAd(context);
+        }
+        startAppAd.loadAd(new AdEventListener() {
             @Override
-            public void onInitializationComplete() {
-                InitializationSuccess();
+            public void onReceiveAd(Ad ad) {
+                AdLoaded();
             }
 
             @Override
-            public void onInitializationFailed(UnityAds.UnityAdsInitializationError error, String message) {
-                InitializationFailed(message);
+            public void onFailedToReceiveAd(Ad ad) {
+                AdFailedToLoad(ad != null ? ad.getErrorMessage() : "Gagal load iklan dari server Start.io");
             }
         });
     }
 
-    @SimpleEvent(description = "Event saat inisialisasi sukses")
-    public void InitializationSuccess() {
-        EventDispatcher.dispatchEvent(this, "InitializationSuccess");
+    @SimpleEvent(description = "Event saat iklan sukses diload")
+    public void AdLoaded() {
+        EventDispatcher.dispatchEvent(this, "AdLoaded");
     }
 
-    @SimpleEvent(description = "Event saat inisialisasi gagal")
-    public void InitializationFailed(String error) {
-        EventDispatcher.dispatchEvent(this, "InitializationFailed", error);
+    @SimpleEvent(description = "Event saat iklan gagal diload")
+    public void AdFailedToLoad(String error) {
+        EventDispatcher.dispatchEvent(this, "AdFailedToLoad", error);
     }
 
-    @SimpleFunction(description = "Load iklan Interstitial")
-    public void LoadInterstitial(String adUnitId) {
-        UnityAds.load(adUnitId, new IUnityAdsLoadListener() {
-            @Override
-            public void onUnityAdsAdLoaded(String placementId) {
-                AdLoaded(placementId);
-            }
+    // 3. TAMPILKAN IKLAN
+    @SimpleFunction(description = "Tampilkan Iklan Interstitial")
+    public void ShowInterstitial() {
+        if (startAppAd != null && startAppAd.isReady()) {
+            startAppAd.showAd(new AdDisplayListener() {
+                @Override
+                public void adHidden(Ad ad) {
+                    AdClosed();
+                }
 
-            @Override
-            public void onUnityAdsFailedToLoad(String placementId, UnityAds.UnityAdsLoadError error, String message) {
-                AdFailedToLoad(placementId, message);
-            }
-        });
-    }
+                @Override
+                public void adDisplayed(Ad ad) {
+                    AdOpened();
+                }
 
-    @SimpleEvent(description = "Event saat iklan berhasil disiapkan")
-    public void AdLoaded(String placementId) {
-        EventDispatcher.dispatchEvent(this, "AdLoaded", placementId);
-    }
+                @Override
+                public void adClicked(Ad ad) {
+                    AdClicked();
+                }
 
-    @SimpleEvent(description = "Event saat iklan gagal disiapkan")
-    public void AdFailedToLoad(String placementId, String error) {
-        EventDispatcher.dispatchEvent(this, "AdFailedToLoad", placementId, error);
-    }
-
-    @SimpleFunction(description = "Tampilkan iklan Interstitial")
-    public void ShowInterstitial(String adUnitId) {
-        UnityAds.show(activity, adUnitId, new UnityAdsShowOptions(), new IUnityAdsShowListener() {
-            @Override
-            public void onUnityAdsShowFailure(String placementId, UnityAds.UnityAdsShowError error, String message) {
-                AdFailedToShow(placementId, message);
-            }
-            @Override
-            public void onUnityAdsShowStart(String placementId) {
-                AdOpened(placementId);
-            }
-            @Override
-            public void onUnityAdsShowClick(String placementId) {
-                AdClicked(placementId);
-            }
-            @Override
-            public void onUnityAdsShowComplete(String placementId, UnityAds.UnityAdsShowCompletionState state) {
-                AdClosed(placementId);
-            }
-        });
+                @Override
+                public void adNotDisplayed(Ad ad) {
+                    AdFailedToShow("Iklan gagal ditampilkan di layar");
+                }
+            });
+        } else {
+            AdFailedToShow("Iklan belum siap (not ready). Pastikan sudah di-load!");
+        }
     }
 
     @SimpleEvent(description = "Event saat iklan gagal tayang")
-    public void AdFailedToShow(String placementId, String error) {
-        EventDispatcher.dispatchEvent(this, "AdFailedToShow", placementId, error);
+    public void AdFailedToShow(String error) {
+        EventDispatcher.dispatchEvent(this, "AdFailedToShow", error);
     }
+
     @SimpleEvent(description = "Event saat iklan terbuka")
-    public void AdOpened(String placementId) {
-        EventDispatcher.dispatchEvent(this, "AdOpened", placementId);
+    public void AdOpened() {
+        EventDispatcher.dispatchEvent(this, "AdOpened");
     }
-    @SimpleEvent(description = "Event saat iklan diklik")
-    public void AdClicked(String placementId) {
-        EventDispatcher.dispatchEvent(this, "AdClicked", placementId);
-    }
+
     @SimpleEvent(description = "Event saat iklan ditutup")
-    public void AdClosed(String placementId) {
-        EventDispatcher.dispatchEvent(this, "AdClosed", placementId);
+    public void AdClosed() {
+        EventDispatcher.dispatchEvent(this, "AdClosed");
+    }
+
+    @SimpleEvent(description = "Event saat iklan diklik")
+    public void AdClicked() {
+        EventDispatcher.dispatchEvent(this, "AdClicked");
     }
 }
